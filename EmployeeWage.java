6d5d68106d256afbb5a7bedcd1c9ba075ba88976
Pancham Mehta <pancham1305@gmail.com>
2024-11-27 17:53:46 +0530

    Initial Commit

---
+++ EmployeeWage.java
@@ -0,0 +1,19 @@
+public class EmployeeWage {
+    public static void main(String[] args) {
+        // We shall go on the main program
+
+    }
+}
+
+/*
+ * Git Procedure
+ * Step by step in terminal
+ * git init (it initializes git inside your current folder): Done one time at
+ * the beginning of the oroject
+ * Stage - Staging
+ * We have to Select the files which we want to keep a record of or keep a track
+ * of or the files which are to be used int he current situation
+ * To select the files the procedure to select files is called staging and to
+ * mark the files which are to be stored, we use git add <FileName>
+ * 
+ */
