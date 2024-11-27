ce842e1f686f3e84b07fbda1624cab5e976c8bb2
Pancham Mehta <pancham1305@gmail.com>
2024-11-27 18:01:25 +0530

    Second

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,6 +1,5 @@
 public class EmployeeWage {
     public static void main(String[] args) {
-        // We shall go on the main program
 
     }
 }
@@ -15,5 +14,17 @@ public class EmployeeWage {
  * of or the files which are to be used int he current situation
  * To select the files the procedure to select files is called staging and to
  * mark the files which are to be stored, we use git add <FileName>
+ * to check the status, we use: git status
+ * Storing the files inside the system - to store the files which are staged, we
+ * have to use a command called git commit- but remember always a commit must be
+ * followed by a message, without commit message we are not allowed to save the
+ * code
+ * commit message must be minimum
+ * // * Changing master branch to main because master gives rise to derogatory
+ * terms such as Slaves
+ * Next: Change we will be renaming master to main (only 1 time)
  * 
+ * We have to provide the remote repository link and save it inside a keyword
+
+ * git remote add origin <URL>
  */
